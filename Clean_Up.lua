8e68a7ec92049d6ae50e5b7c4e82b3dfe6b4e7d9
Manuel Simon Hirsig <simon.hirsig@gmail.com>
2016-10-31 17:28:56 +0100

    refactoring

--- Clean_Up.lua
+++ Clean_Up.lua
@@ -311,20 +311,6 @@ function UpdateButton(key)
 	button:Show()
 end
 
-
-local strataOrder = {
-	UNKNOWN = -1,
-	WORLD = 0,
-	BACKGROUND = 1,
-	LOW = 2,
-	MEDIUM = 3,
-	HIGH = 4,
-	DIALOG = 5,
-	FULLSCREEN = 6,
-	FULLSCREEN_DIALOG = 7,
-	TOOLTIP = 8,
-}
-
 function CreateButtonPlacer()
 	local frame = CreateFrame('EditBox', nil, UIParent)
 	buttonPlacer = frame
